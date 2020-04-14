d716260d2c47b571433ffa3bccb695ccfae8ea05
Aarth Thakore <aarth.thakore@gmail.com>
2020-04-14 10:02:32 -0400

    testing out write code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Date test;
+        test = new Date(2020, 4, 13);
+        System.out.println(test.toString());
+    }
+}
