3237f035e11733f77650b306c00a264c5c9cfae8
Aaron Griffin <griffin.aaron.r@gmail.com>
2020-04-15 12:47:41 -0400

    Made passenger and ticket forms

--- src/view/BusRideSelectionForm.java
+++ src/view/BusRideSelectionForm.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,6 +10,8 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
+import java.util.Objects;
+
 public class BusRideSelectionForm {
 	private static ChoiceBox<String> destination;
 	private static ChoiceBox<String> origin;
@@ -16,16 +19,36 @@ public class BusRideSelectionForm {
 	private static ChoiceBox<String> departureTime;
 	private static Text etaResult;
 
-	public static Scene createScene() {
+	private static String previousDestination;
+	private static Scene scene;
+
+	public static Scene getScene() {
+		if(scene == null)
+			scene = createScene();
+		return scene;
+	}
+
+	private static Scene createScene() {
 		// Creating elements
 		var title = new Text("Pick the bus stops you will depart and arrive at");
 		var destinationText = new Text("Destination");
 		destination = new ChoiceBox<>();
+		destination.getItems().add("Atlanta");
+		destination.getItems().add("Orlando");
 		var originText = new Text("Origin");
 		origin = new ChoiceBox<>();
 		var departureText = new Text("Departure Time");
 		departureDate = new DatePicker();
 		departureTime = new ChoiceBox<>();
+		for(int pm = 0; pm <= 1; pm++) {
+			for(int hr = 0; hr < 12; hr++) {
+				for(int min = 0; min < 60; min += 30) {
+					String suffix = (pm == 1 ? " PM" : " AM");
+					String prefix = (hr == 0 ? "12:" : hr + ":");
+					departureTime.getItems().add(prefix + String.format("%02d", min) + suffix);
+				}
+			}
+		}
 		var etaText = new Text("ETA");
 		etaResult = new Text("--:--");
 		var nextButton = new Button("Next");
@@ -55,19 +78,26 @@ public class BusRideSelectionForm {
 		gridPane.add(etaText, 0, 4);
 		gridPane.add(etaResult, 1, 4, 2, 1);
 		gridPane.add(nextButton, 1, 5);
+		update();
 
 		return new Scene(gridPane);
 	}
 
-	public static void update() {
-
+	private static void update() {
+		String newDestination = destination.getValue();
+		if(!Objects.equals(previousDestination, newDestination)) {
+			origin.setDisable(newDestination == null);
+			origin.setValue(null);
+			ObservableList<String> list = origin.getItems();
+			list.clear();
+			list.addAll(destination.getItems());
+			list.remove(newDestination);
+			previousDestination = newDestination;
+			origin.setItems(list);
+		}
 	}
 
-	public static void moveToPassengerForm() {
-		if(true) {
-			PopupApp.displayErrorMessage("You fail.");
-			return;
-		}
-		BusTicketApp.setScene(PassengerForm.createScene());
+	private static void moveToPassengerForm() {
+		BusTicketApp.setScene(PassengerForm.getScene());
 	}
 }
